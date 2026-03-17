a7400e34e463b98e957191326c5fe3020d7c7f3c
doit <1795973398@qq.com>
2026-03-18 02:19:48 +0800

    fix: support both GitHub Pages and Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hardware-testing-on-web/',
+  base: process.env.VITE_BASE_URL ?? '/',
   plugins: [react()],
   server: {
     port: 3000,
